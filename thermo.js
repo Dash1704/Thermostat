acd15531219346cb6dc3b1d48ffe3768befd32b7
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-03-04 12:02:41 +0000

    added the max temperature

--- thermo.js
+++ thermo.js
@@ -2,18 +2,35 @@ class Thermostat {
 
   constructor() {
     this.defaultTemperature = 20
+    this.maxiumTemperature = 25
+    this.powerSavingMode = true
   }
 
   getTemperature(){
-    return this.defaultTemperature
-  }
+    // return this.defaultTemperature
+    // if (this.defaultTemperature > 25){
+    //   return this.maxiumTemperature
+    // };
+    if (this.powerSavingMode === true && this.defaultTemperature > 25){
+      return this.maxiumTemperature
+    }
+    else {return this.defaultTemperature
+    }
+  };
 
   up() {
     return this.defaultTemperature += 1
-  }
+  };
 
   down() {
     return this.defaultTemperature -= 1
-  }
-}
+  };
+
+  setPowerSavingMode(onOrOff) {
+    this.powerSavingMode = onOrOff
+  };
+
+};
+
+
 module.exports = Thermostat
--- thermo.test.js
+++ thermo.test.js
@@ -1,16 +1,19 @@
+
 const Thermostat = require('./thermo')
 
 describe(Thermostat, () => {
-  const thermostat = new Thermostat();
-  // const thermoDouble = { defaultTemperature: () => 20 };
-
+  
   describe('getTemperature', () => {
+    let thermostat = new Thermostat
+
     it('should give default temperature as 20', () => {
     expect(thermostat.getTemperature()).toEqual(20)
     });
   });
 
   describe('up', () => {
+    let thermostat = new Thermostat
+
     it('should increase temperature by 2', () => {
       thermostat.up()  
       thermostat.up() 
@@ -19,9 +22,34 @@ describe(Thermostat, () => {
   });
 
   describe('down', () => {
+    let thermostat = new Thermostat
+
     it('should decrease temperature by 1', () => {
       thermostat.down()
-      expect(thermostat.getTemperature()).toEqual(21)
+      expect(thermostat.getTemperature()).toEqual(19)
     });
+  });
+
+  describe('setPowerSavingMode true', () => {
+    let thermostat = new Thermostat
+
+    it('should be on by default', () => {
+      expect(thermostat.powerSavingMode).toEqual(true)
+    });
+
+    it('should be able to switch off', () => {
+      thermostat.setPowerSavingMode(false)
+      expect(thermostat.powerSavingMode).toEqual(false)
+    });
+
+    it('should set maxium temperature of 25', () => {
+      thermostat.setPowerSavingMode(true)
+      for (let i = 0; i < 10; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.getTemperature()).toBe (25);
+    });
+
+
   });
 });
